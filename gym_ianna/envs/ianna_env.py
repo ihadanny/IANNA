2089b0df83d5ce68baaa7b479c570744e0186d65
ihadanny <ido.hadanny@gmail.com>
2017-07-29 17:44:13 +0300

    workable version of 2 action - filter and group

--- gym_ianna/envs/ianna_env.py
+++ gym_ianna/envs/ianna_env.py
@@ -4,19 +4,15 @@ import gym
 from gym import spaces
 from gym.utils import seeding
 import numpy as np, pandas as pd, os
-import pickle 
 
 logger = logging.getLogger(__name__)
 
 class IANNAEnv(gym.Env):
 
-    def get_current_state(self):
-        res = [self.data.shape[0]]
-        dist_counts = self.data.nunique().to_dict()
-        for col in self.columns:
-            res.append(dist_counts[col])
-        return np.array(res)
-
+    metadata = {
+        'render.modes': ['human'],
+    }
+    
     def __init__(self):
 
         #    e.g. Nintendo Game Controller
@@ -28,52 +24,92 @@ class IANNAEnv(gym.Env):
         #        MultiDiscrete([ [0,4], [0,1], [0,1] ])
 
         #        IANNA actions would be:
-        #        1) action_type:            filter[0], group[1]
-        #        2) field_id:               [0..num_of_fields-1]
-        #        3) filter_operator:        EQ[0], GT[1]. LT[2] if the selected field was numeric (maybe change semantics if field is STR?)
-        #        4) filter_decile:          [0..9] the filter operand  
-        #        5) aggregation field_id:   [0..num_of_fields-1] (what do we do if the selected field is also the main field_id?)
-        #        6) aggregation type:       MEAN[0], COUNT[1], SUM[2], MIN[3], MAX[4]
+        #        0) action_type:            filter[0], group[1]
+        #        1) col_id:                 [0..num_of_columns-1]
+        #        2) filter_operator:        LT[0], GT[1] if the selected column was numeric (maybe change semantics if column is STR?)
+        #        3) filter_decile:          [0..9] the filter operand  
+        #        4) aggregation column_id:  [0..num_of_columns - 1] (what do we do if the selected col is also grouped_by?)
+        #        5) aggregation type:       MEAN[0], COUNT[1], SUM[2], MIN[3], MAX[4]
         dir = os.path.dirname(__file__)
         self.filename = os.path.join(dir, '../../data/1.tsv')        
         self.data = pd.read_csv(self.filename, sep = '\t')
         self.columns = self.data.columns
-        print(self.columns)
         self.num_rows = self.data.shape[0]
-        self.num_fields = self.data.shape[1]
+        self.num_columns = self.data.shape[1]
+        self.grouped_by = {col: 0 for col in self.columns}
         
-        self.action_space = spaces.MultiDiscrete([[0,1], [0, self.num_fields-1], [0, 2], [0, 9], [0, self.num_fields-1], [0,4]])
+        self.action_space = spaces.MultiDiscrete([[0,1], [0, self.num_columns-1], [0, 2], [0, 9], [0, self.num_columns-1], [0,4]])
         
         #   Two kinds of valid input:
         #   Box(-1.0, 1.0, (3,4)) # low and high are scalars, and shape is provided
         #   Box(np.array([-1.0,-2.0]), np.array([2.0,4.0])) # low and high are arrays of the same shape
 
         #   IANNA observations would be:
-        #   1) number of records
-        #   2..num_fields) distinct values of every field        
+        #   number of records
+        #   number of groups
+        #   distinct values of every column
+        #   grouped_by indication for each column
+        
         
         high = self.get_current_state()
         low = np.zeros_like(high)
         self.observation_space = spaces.Box(low, high)        
     
+    def get_current_state(self):
+        res = [self.data.shape[0]]
+        group_cols = [k for k, v in self.grouped_by.items() if v == 1]
+        if len(group_cols) > 0:
+            num_groups = self.data[group_cols].drop_duplicates().shape[0]
+        else:
+            num_groups = self.data.shape[0]
+        res.append(num_groups)
+        dist_counts = self.data.nunique().to_dict()
+        for col in self.columns:
+            res.append(dist_counts[col])
+        for col in self.columns:
+            res.append(self.grouped_by[col])
+        
+        return np.array(res)
 
     def _step(self, action):
         assert self.action_space.contains(action), "%r (%s) invalid"%(action, type(action))
+        operator_type = OPERATOR_TYPE_LOOKUP[action[0]] 
         col = self.columns[action[1]]
-        if action[0] == 0:
-            print('filter', col)
-        else:
+        if operator_type == 'filter':
+            filter_operator = FILTER_OPERATOR_LOOKUP[action[2]]
+            operand = 1.0 * self.data[col].count() * action[3]/10
+            print('filter', col, filter_operator, operand)
+            if filter_operator == "LT":
+                self.data = self.data[self.data[col].rank() < operand]
+            elif filter_operator == "GT":
+                self.data = self.data[self.data[col].rank() > operand]
+            else:
+                raise Exception("unknown filter operator: " + filter_operator)
+        elif operator_type == 'group':
             print('group', col)
-        
+            self.grouped_by[col] = 1
+        else:
+            raise Exception("unknown operator type: " + operator_type)
+            
         reward = 1.0
         return self.get_current_state(), reward, False, {}
 
     def _reset(self):
         self.data = pd.read_csv(self.filename, sep = '\t')
+        self.grouped_by = {col: 0 for col in self.columns}
         return np.array(self.get_current_state())
 
     def _render(self, mode='human', close=False):
+        print(self.data.nunique().T)
         return None
 
+OPERATOR_TYPE_LOOKUP = {
+        0: "filter",
+        1: "group",
+        }
 
+FILTER_OPERATOR_LOOKUP = {
+        0: "LT",
+        1: "GT",
+        }
 
